198ed7b0271fea0bce143d29cede49d345867ab1
Sidharth-R-07 <sidhuram365@gmail.com>
2024-09-10 17:34:44 +0530

    ADDED NESSCERY PACKAGES AND CONNECTED FIREBASE

---
+++ lib/core/utils/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0C54BE);
+  static const Color secondary = Color(0xFF303F60);
+  static const Color white = Color(0xFFF5F9FD);
+  static const Color grey = Color(0xffced3dc);
+}
